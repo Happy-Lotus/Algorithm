731913f4f972fe1bf3bd4abcd3bdea5ebad8ca7f
HeeYeon-Kim <62700196+Happy-Lotus@users.noreply.github.com>
2024-06-25 16:42:31 +0900

    [Silver II] Title: 스택 수열, Time: 336 ms, Memory: 27356 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,42 +1,43 @@
-import org.w3c.dom.Node;
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
 
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringBuilder sb = new StringBuilder();
     void run(){
         try {
             int N = Integer.parseInt(br.readLine());
-            int [] A = new int[N];
-            Stack<Integer>stack = new Stack<>();
-            for(int i=0;i<N;i++){
-                A[i]=Integer.parseInt(br.readLine());
-            }
-            int c = 1;
+            int [] arr = new int[N];
+            Stack<Integer> stack = new Stack<>();
+            int num=1;
 
-            for(int i=0;i<A.length;i++){
-                int num = A[i];
-                if(c<=num){
-                    while(c<=num){
-                        stack.push(c++);
+            for(int i=0;i<N;i++){
+                arr[i] = Integer.parseInt(br.readLine());
+            }
+
+            for(int i=0;i<N;i++){
+                int temp = arr[i];
+                if(temp>=num){
+                    while(temp>=num){
+                        stack.push(num++);
                         sb.append("+\n");
                     }
                     stack.pop();
                     sb.append("-\n");
                 }else{
                     int n = stack.pop();
-                    if(n>num){
-                        System.out.print("NO");
-                        return;
+                    if(n>temp){
+                        sb.delete(0,sb.length());
+                        sb.append("NO");
+                        break;
                     }else{
                         sb.append("-\n");
                     }
                 }
             }
-            System.out.print(sb);
+            System.out.println(sb);
         }catch(Exception e){}
     }
 
